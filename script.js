da1d7f802ce760208f340b4241b9b740d7c51aa4
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-03-11 14:46:41 -0400

    version 0.1 initiation

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 // Assignment Code
+
+
 var generateBtn = document.querySelector("#generate");
 
+
+function generatePassword(){
+
+
+var numChar = prompt("How many characters would you like to use");
+console.log(numChar);
+
+
+
+
+  return "numChar"
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
