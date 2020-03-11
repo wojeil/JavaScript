e43dfe6b9f8b70ca48001dc8e3a4db8cb56b00c6
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-03-11 17:33:17 -0400

    Version 0.2 Array set for password bank

--- script.js
+++ script.js
@@ -4,16 +4,18 @@
 var generateBtn = document.querySelector("#generate");
 
 
-function generatePassword(){
+function generatePassword() {
+  var X = ["A","B","C","D","E","F", "G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","Z","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","1","2","3","4","5","6","7","8","9","0","!","@","#","$","%","^","&","*","(",")","_","+"]; //array character bank named X.
+  
+  var numChar = prompt("How many characters would you like to use");
+  console.log(numChar);
+  
+  for (let index = 0; index < X.length; index++) {
 
+    
+    
+  }
 
-var numChar = prompt("How many characters would you like to use");
-console.log(numChar);
-
-
-
-
-  return "numChar"
 }
 // Write password to the #password input
 function writePassword() {
