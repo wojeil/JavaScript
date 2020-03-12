9118f522c7f4acf7a8c561dbf9b0886f28d0727b
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-03-11 21:57:10 -0400

    version 0.4 seperated my arrays into 4

--- script.js
+++ script.js
@@ -2,16 +2,23 @@
 
 
 var generateBtn = document.querySelector("#generate");
+//array character bank named X. split the array into 4 arrays.
+var upper = ["A","B","C","D","E","F", "G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","Z","Y","Z"]; 
+var lower =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var number =["1","2","3","4","5","6","7","8","9","0",];
+var special =["!","@","#","$","%","^","&","*","(",")","_","+"];
+
+//password set up variable
+var fP = "";
 
 
 function generatePassword() {
-  var X = ["A","B","C","D","E","F", "G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","Z","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","1","2","3","4","5","6","7","8","9","0","!","@","#","$","%","^","&","*","(",")","_","+"]; //array character bank named X.
-  
-  //prompts and confirms added
+ //prompts and confirms added
   var nC1 = prompt("How many characters would you like to use");
   var nC2 = confirm("Do you want a lower case character?");
   var nC3 = confirm("Do you want an UPPER CASE CHARACTER?");
-  var nC4 = confirm("Do you want any special Characters?");
+  var nC4 = confirm("Do you want to use any numbers");
+  var nC5 = confirm("Do you want any special Characters?");
 
   console.log(nC1);
   console.log(nC2);
@@ -21,7 +28,7 @@ function generatePassword() {
   
  
  // for loop 
-  for (let index = 0; index < X.length; index++) {
+  for (let index = 0; index < fP.length; index++) {
 
     
     
